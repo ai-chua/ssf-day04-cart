27df204bad629040c34b26457d25f42a5170a614
Ailin Chua <ai-chua@outlook.com>
2020-10-29 22:04:24 +0800

    allowed different users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Load required libraries from node_modules
 const express = require('express')
 const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 // Configure the environment
 const PORT = parseInt(process.env.PORT) || 3000
@@ -17,15 +18,37 @@ app.set('view engine', 'hbs')
 app.set('views', __dirname + '/views')
 
 // Configure the static files
-app.use(
-  express.static(__dirname + '/static')
-  )
+app.use(express.static(__dirname + '/static'))
+
 
 // Prefix match
 app.get(['/', '/index.html'], (req, res) => {
+  const cart = []
+  // cart.push(localStorage.getItem('currentCart'))
   res.status(200)
   res.type('text/html')
-  res.render('index')
+  res.render('index', { cartState: JSON.stringify(cart) })
+})
+
+
+app.post(['/', '/index.html'],
+    express.urlencoded({ extended: true }),
+    (req, res) => {
+    const cart = JSON.parse(req.body.cartState)
+    // const cart = JSON.parse(req.body.shoppingList)
+    cart.push({
+      item: req.body.item.toLowerCase(),
+      quantity: parseInt(req.body.quantity, 10),
+      unitPrice: parseFloat(req.body.unitprice)
+    })
+    console.log(cart)
+
+    res.status(201)
+    res.type('text/html')
+    res.render('index', {
+      cart: cart,
+      cartState: JSON.stringify(cart)
+    })
 })
 
 // Start express
